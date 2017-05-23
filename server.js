f4f9011ab90f78924c7ef7012b36baeb98bf2dbd
Dusan Vesic <dusan.vesic@devtech.local>
2017-05-23 13:48:28 +0200

    Init commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({ 
+    port: process.env.PORT || 3000,
+});
+
+server.register(require('inert'), (err) => {
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
