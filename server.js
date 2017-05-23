94e410a476cb017595d37aa262382171bf0d7726
Dusan Vesic <dusan.vesic@devtech.local>
2017-05-23 14:30:49 +0200

    Resolve A records

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+const dns = require('dns');
 
-server.connection({ 
-    port: process.env.PORT || 3000,
+server.connection({
+  port: process.env.PORT || 3000,
 });
 
 server.register(require('inert'), (err) => {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err;
+  }
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply.file('./public/index.html');
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file('./public/index.html');
+    }
+  });
 });
 
 server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
+  method: 'GET',
+  path: '/hello',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/tools/dns',
+  handler: function (request, reply) {
+    let url = encodeURIComponent(request.query.url);
+    dns.resolve4(url, (err, addresses) => {
+      reply({'A': addresses});
+    });
+  }
 });
 
 server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
 });
